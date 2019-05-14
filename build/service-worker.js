bc3272136180a07ef0d3d4333b1bb1728a85583d
kirillAkutin <jqweryjson@gmail.com>
2019-05-14 19:16:33 +0300

    sdf

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jqweryjson/TL2/precache-manifest.117cf00fc3aaaf98b775f7d78420470b.js"
+  "/TL2/precache-manifest.478485afa65b8ecf96ce49456816bb71.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/jqweryjson/TL2/index.html", {
+workbox.routing.registerNavigationRoute("/TL2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
