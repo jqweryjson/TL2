315aa50a5c7136fe2092d7cffda60ee3875ee1e3
kirillAkutin <jqweryjson@gmail.com>
2019-05-15 13:52:19 +0300

    return vueport

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TL2/precache-manifest.a9f78aa51a6a03cd9dfa5f748822428c.js"
+  "/TL2/precache-manifest.823cc20e38a924e4f535ea2d52a5925e.js"
 );
 
 workbox.clientsClaim();
