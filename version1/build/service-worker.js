d9f3a83b22f67936b52d4d5355b54a872b69799a
kirillAkutin <jqweryjson@gmail.com>
2019-07-18 18:37:26 +0300

    #6270 - Кнопка "Назад" для прототипов

--- version1/build/service-worker.js
+++ version1/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/1/precache-manifest.9587594f86c0655e7d02a659b4fa2100.js"
+  "/TL2/precache-manifest.a3f946522e904318f3c56c9f012ebbc8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/1/index.html", {
+workbox.routing.registerNavigationRoute("/TL2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
